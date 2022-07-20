b809b7422979f7f379b7760a94d590c4d06f5536
michael <jonesmic014@gmail.com>
2022-07-20 05:10:36 -0400

    Create RPS.c
    - Also created welcome function

---
+++ RPS.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void welcome(); // Function Declaration
+
+int main (void)
+{
+    // Local Declarations
+
+    // Statements
+    welcome(); // function call
+
+    return 0;
+    // main
+}
+
+void welcome()
+{
+    // Statements
+    printf("Welcome to the Rock/Paper/Scissors Game!\n\n");
+}
