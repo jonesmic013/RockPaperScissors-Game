372ae55da4318a4d5b238aeaaa5ac7cf3517e64a
michael <jonesmic014@gmail.com>
2022-07-22 03:35:18 -0400

    Add functions
    - printMove
    - compareMoves
    - printResults
    - goodbye

--- RPS.c
+++ RPS.c
@@ -4,6 +4,10 @@
 void welcome(); // Function Declaration
 int getNumRounds(); // Function Declaration
 int playRound(); // Function Declaration
+void printMove(int move); // Function Declaration
+int compareMoves(int playerInput, int comInput); // Function Declaration
+void printResults(int round, int playerWins, int comWins); // Function Declaraton
+void goodbye(); // Function Declaration
 
 int main (void)
 {
@@ -11,7 +15,8 @@ int main (void)
     int numRounds; // max rounds to play (aka best of ODD_NUMBER)
     int playerWins; // round wins by user
     int comWins; // round wins by computer
-    int roundWinner; // round winner, 0/1 for computer/player
+    int ties; // number of ties
+    int roundWinner; // round winner, 1/2 for player/computer, 0 for tie
     int round; // current round number
 
     // Statements
@@ -22,19 +27,28 @@ int main (void)
     {
         printf("\n--- Round #%d ---\n", round);
         roundWinner = playRound(); // function call
-        if (roundWinner) // player won round
+        if (roundWinner == 1) // player won round
         {
-            printf("\nRound %d winner: YOU\nPress ENTER to continue.", round);
+            printf("\nRound %d winner: PLAYER\nPress ENTER to continue.", round);
             playerWins++;
+            round++;
         }
-        else // computer won round
+        else if (roundWinner == 1) // computer won round
         {
             printf("\nRound %d winner: COMPUTER\nPress ENTER to continue.", round);
             comWins++;
+            round++;
+        }
+        else
+        {
+            printf("\nRound %d winner: TIE\nReplaying round...\nPress ENTER to continue.", round);
+            ties++;
         }
         getchar();
-        round++;
+        getchar();
     } while (playerWins <= (numRounds / 2) || playerWins <= (comWins / 2));
+
+    goodbye();
 
     return 0;
     // main
@@ -75,7 +89,7 @@ int getNumRounds()
 int playRound()
 {
     // Local Declarations
-    int roundWinner; // round winner, 0/1 for computer/player
+    int roundWinner; // round winner, 1/2 for player/computer, 0 for tie
     int playerInput; // move chosen by player
     int comInput; // move randomly chosen by computer
 
@@ -87,20 +101,76 @@ int playRound()
         printf("[1] Rock\n[2] Paper\n[3] Scissors\n-> ");
         scanf("%d", &playerInput);
 
-        if (playerInput < 0 || playerInput > 4)
+        if (playerInput < 1 || playerInput > 3)
         {
             printf("Invalid option!\n\n");
         }
-    } while (playerInput < 0 || playerInput > 4); // must choose 1-3
-    
+    } while (playerInput < 1 || playerInput > 3); // must choose 1-3
+
     // get random move for computer
     comInput = rand() % 4 + 1;
-    printf("\nCOM CHOSE: %d\n", comInput);
 
     // print what both people chose
-    printf("YOU chose: ROCK\n");
-    printf("COMPUTER chose: ROCK");
+    printf("You chose: ");
+    printMove(playerInput); // function call
+    printf("COMPUTER chose: ");
+    printMove(comInput); // function call
 
-    roundWinner = 0;
+    // now get the round winner
+    roundWinner = compareMoves(playerInput, comInput); // function call
+
     return roundWinner; // return winner of round
+}
+
+void printMove(int move)
+{
+    // Statements
+    switch (move)
+    {
+        case 1: printf("ROCK\n");
+                break;
+        case 2: printf("PAPER\n");
+                break;
+        case 3: printf("SCISSORS\n");
+                break;
+    }
+}
+
+int compareMoves(int playerInput, int comInput)
+{
+    // Local Declarations
+    int roundWinner; // round winner, 1/2 for player/computer, 0 for tie
+
+    // Statements
+    roundWinner = 2; // default to computer winning
+
+    // check for tie
+    if (playerInput == comInput)
+    {
+        roundWinner = 0;
+    }
+
+    // check all 3 conditions where the player beats the computer
+    if ((playerInput == 1 && comInput == 3) || (playerInput == 2 && comInput == 1) || (playerInput == 3 && comInput == 2))
+    {
+        roundWinner == 1;
+    }
+
+    return roundWinner;
+}
+
+void printResults(int round, int playerWins, int comWins, int ties)
+{
+    // Statements
+    printf("\n--------------------");
+    printf("\nRounds played: %d", round);
+    printf("\nPlayer wins: %d\nComputer wins: %d\nTies: %d", playerWins, comWins, ties);
+    printf("\n--------------------");
+}
+
+void goodbye()
+{
+    // Statements
+    printf("\n\nThanks for playing!\n");
+    printf("Press ENTER to exit.");
 }
